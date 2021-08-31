0cfa87837455fee7d9f150d2c053a2339d91dec8
daraprokaseva <d@p.com>
2021-08-31 18:42:46 +0300

    rm

--- src/com/company/Dog.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-public class Dog extends Animal{
-    public Dog(String name) {
-        super("Пес", name, 1000, 4, 20);
-    }
-}
--- src/com/company/Obstacle.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-import com.company.Animal;
-import com.company.Competitor;
-
-public abstract class Obstacle {
-    public abstract void doIt(Competitor competitor);
-}
